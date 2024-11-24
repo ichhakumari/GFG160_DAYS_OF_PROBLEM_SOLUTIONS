ecbaba0338a77b46b61a598a2982d02737201d05
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-11-24 20:20:44 +0530

    Update 2_pushZerosToEnd.java

--- 2_pushZerosToEnd.java
+++ 2_pushZerosToEnd.java
@@ -1,8 +1,23 @@
-// Java Program to move all zeros to end using one traversal
+/** Given an array arr[]. Push all the zeros of the given array to the right end of the array while maintaining the order of non-zero elements. Do the mentioned change in the array in place.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+Input: arr[] = [10, 20, 30]
+Output: [10, 20, 30]
+Explanation: No change in array as there are no 0s.
+Input: arr[] = [0, 0]
+Output: [0, 0]
+Explanation: No change in array as there are all 0s.
+  
+*/
+
 
 import java.util.Arrays;
 
-class GfG {
+class Solution {
 
     // Function which pushes all zeros to end of array
     static void pushZerosToEnd(int[] arr) {
@@ -12,7 +27,6 @@ class GfG {
 
         for (int i = 0; i < arr.length; i++) {
 
-            // If the current element is non-zero
             if (arr[i] != 0) {
 
                 // Swap the current element with the 0 at index 'count'
